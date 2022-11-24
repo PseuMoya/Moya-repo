9bb8ca92f1eb1b339ad9caee9e749e7c6a763a80
Pseu_Moya <116236683+PseuMoya@users.noreply.github.com>
2022-11-24 15:28:53 +0800

    Add files via upload

---
+++ FileReader_Moya/FrmStudentRecord.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileReader_Moya
+{
+    public partial class FrmStudentRecord : Form
+    {
+        public FrmStudentRecord()
+        {
+            InitializeComponent();
+        }
+        public void DisplaytoList()
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string _getText = "";
+                while ((_getText = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(_getText);
+                    lvView.Items.Add(_getText);
+                }
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            FrmRegistration frmRegistration = new FrmRegistration();
+            frmRegistration.Show();
+
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            DisplaytoList();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully Uploaded");
+            lvView.Clear();
+        }
+    }
+}
